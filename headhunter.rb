795068ccdc67fffd61389f6d17e4981adfbf44a7
André Wendt <awendt@freeshell.org>
2010-03-24 19:40:01 +0100

    Added a homepage with Google-O-Meter and everything

--- headhunter.rb
+++ headhunter.rb
@@ -20,11 +20,12 @@ helpers do
     avatar_url = JSON.parse(user_info)["profile_image_url"]
     avatar_url ||= "http://static.twitter.com/images/default_profile_bigger.png"
   end
-end
 
-get '/status' do
-  status = Net::HTTP.get(URI.parse("http://api.twitter.com/1/account/rate_limit_status.json"))
-  "Remaining hits today: #{JSON.parse(status)['remaining_hits']}"
+  def google_chart
+    ["http://chart.apis.google.com/chart?chs=400x250", "cht=gom", "chd=t:#{(@plot_value)}",
+      "chco=FF0000,FF8040,FFFF00,00FF00,00FFFF,0000FF,800080", "chxt=x,y",
+      "chxl=0:||1:|low|normal|plenty"].join("&")
+  end
 end
 
 get '/favicon.ico' do
@@ -41,4 +42,15 @@ get '/:user' do
     CACHE.set(user, avatar_url)
   end
   redirect avatar_url
+end
+
+get '/' do
+  status_json = Net::HTTP.get(URI.parse("http://api.twitter.com/1/account/rate_limit_status.json"))
+  status = JSON.parse(status_json)
+  @remaining_hits = status['remaining_hits'].to_i
+  @reset_to = status['hourly_limit']
+  @reset_in_minutes = (status['reset_time_in_seconds'].to_i - Time.now.to_i)/60
+  @plot_value = @reset_in_minutes == 0 ? 50 : (@remaining_hits * 1.0 / @reset_in_minutes * 20).round
+  content_type 'text/html', :charset => 'utf-8'
+  erb :index
 end
--- spec/headhunter_spec.rb
+++ spec/headhunter_spec.rb
@@ -12,5 +12,26 @@ describe "Headhunter" do
     last_response.should be_not_found
   end
 
-end
+  describe "Homepage" do
 
+    it "should respond" do
+      get '/'
+      last_response.should be_ok
+    end
+
+    it "should show the remaining hits allowed by the Twitter API" do
+      Net::HTTP.should_receive(:get).and_return({
+        :reset_time_in_seconds => "1269200600",
+        :remaining_hits => "123",
+        :hourly_limit => "150"
+      }.to_json)
+      Time.should_receive(:now).and_return(now = mock('now', :to_i => 1269200000))
+      get '/'
+      last_response.body.should =~ /123/
+      last_response.body.should =~ /reset to 150/
+      last_response.body.should =~ /10[^0-9]+minutes/
+    end
+
+  end
+
+end
